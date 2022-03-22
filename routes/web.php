556ea0b1bcf56185c806b08d0d10f8e6a189dffe
Little Penguin <20FTT1818@student.pb.edu.bn>
2022-03-22 13:02:41 +0700

    initial commit1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'under-construction'], function () {
+    Route::get('/as2', function () {
+        return view('welcome');
+    });
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
